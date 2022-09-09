1d3efa8b1b9be5ed7a37a37dadf6aaf3c33b2f95
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-09 12:39:02 +0000

    Varibales

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,11 +26,11 @@ int main(void)
 	if (lastnum == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastnum);
-	}	
+	}
 	if (lastnum < 6 && lastnum != 0)
-	{	
+	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnum);
-	}	
+	}
 
 	return (0);
 
