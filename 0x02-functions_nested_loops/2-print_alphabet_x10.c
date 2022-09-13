c58e9d917b0891ecc674020c5b86fd232e81079d
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-13 16:48:42 +0000

    Print lowercase alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,14 @@
 void print_alphabet_x10(void)
 {
 	char alphabet;
-	
+
 	int count;
 
 	count = 0;
 
 	while (count < 10)
 	{
-		for (alphabet = 'a'; alphabet <= 'z'; alphabet ++)
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			_putchar(alphabet);
 		}
