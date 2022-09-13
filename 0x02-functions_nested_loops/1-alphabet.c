4ec249876308d0882e45a4d5c12ecb6df814dba4
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-13 16:22:32 +0000

    Print Lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
  * Return: Always 0
  *
  */
-int main(void)
+void print_alphabet(void)
 {
 	char alphabet;
 	alphabet = 'a';
@@ -17,5 +17,4 @@ int main(void)
 		alphabet++;
 	}
 	_putchar('\n');
-	return (0);
 }
