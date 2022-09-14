d4c5b411190db9453615f1001178a40b816c9cfd
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-14 17:04:54 +0000

    Times-table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,36 +12,24 @@ void times_table(void)
 	int b;
 	int c;
 
-	for(a = 0; a <= 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar('0');
+		for (b = 1; b <= 9; b++)
 		{
+			_putchar(',');
+			_putchar(' ');
 			c = a * b;
 
-			if ((c / 10) == 0)
+			if (c <= 9)
 			{
-				if (b == 0)
-				{
-					_putchar('0');
-				}
-
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(' ');
 			}
-
 			else
 			{
 				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
 
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+	     			_putchar((c % 10) + '0');
 			}
 		}
 		_putchar('\n');
