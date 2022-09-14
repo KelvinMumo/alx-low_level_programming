d2f8f6fe624f427c473b0e2426427b96f0eca8c5
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-14 20:26:16 +0000

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,6 +23,7 @@ void times_table(void)
 				_putchar(c + '0');
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
 			}
 			else
 			{
@@ -30,9 +31,8 @@ void times_table(void)
 				_putchar((c % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
-			}	
+			}
 		}
-
 			_putchar('\n');
 	}
 }
