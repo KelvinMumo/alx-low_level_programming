4d54ef8e18470ff40aac1de509ca8c6bf1648c27
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-15 15:47:18 +0000

    Fizz Buzz git cr git add  . git ad
    2-mul
    
    exit
    
    cd
    cd /.nuo,mgyikm
    
    kjbgvyif/iglbndfjklb nflb'
    nsklbns;dmb;hetwtp
    Vvfdb.,dm

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ *  main - Fizz Buzz
+ *
+ *  Return: 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) != 0))
+		{
+			printf("Fizz");
+			putchar(' ');
+		}
+
+		if (((i % 3) != 0) && ((i % 5) == 0))
+		{
+			printf("Buzz");
+			putchar(' ');
+		}
+
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			printf("FizzBuzz");
+			putchar(' ');
+		}
+	        if (((i % 3) != 0) && ((i % 5) != 0)) 
+		{
+			printf("%i", i);
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
