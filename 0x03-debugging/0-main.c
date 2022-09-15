033dafc40a1f9cc496e3996759d3c49b707aaadd
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-15 09:13:32 +0000

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Check whether integer is positive or negative
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	i = 8;
+
+	positive_or_negative(i);
+
+	return (0);
+}
