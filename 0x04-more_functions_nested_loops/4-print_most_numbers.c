c1d854006af9d468769712eacd1b25428bc733cc
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-15 13:31:23 +0000

    Except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,15 +10,19 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
+	n = 0;
+
+	while (n <= 9)
 	{
-		if (n != 2 && n != 4)
+		if ((n != 2) && (n != 4))
 		{
 			_putchar(n + '0');
+			n++;
 		}
 		else
 		{
 			n++;
 		}
+	}
 	_putchar('\n');
 }
