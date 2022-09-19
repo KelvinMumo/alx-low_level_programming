2958c0b60649fb2feed368b0170a54825c547cea
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-19 15:08:05 +0000

    I,ve got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse order
+ *
+ * @s: String
+ *
+ * Return: void
+ *
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
