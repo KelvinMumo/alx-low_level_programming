5fcec1d805593197cc91222e4b6bf10595051a79
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-19 14:53:45 +0000

    strLEN

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strl - Returns the length of a string
+ *
+ * @s: String
+ *
+ * Return: 0
+ *
+ */
+int _strlen(char *s)
+{
+	int i, count;
+
+	i = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		i++;
+	}
+
+	return (i);
+}
