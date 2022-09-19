29a204c51cf89bf9bd8778a5b888719cda39aaf4
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-19 20:10:14 +0000

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,16 +12,24 @@ int _atoi(char *s)
 {
 	int i = 1;
 
-	unsigned int num = 0;
-	do
-       	{
+	unsigned int n = 0;
+
+	do {
 		if (*s == '-')
+		{
 			i *= -1;
+		}
+
 		else if (*s >= '0' && *s <= '9')
-			num = num * 10 + (*s - '0');
-		else if (num > 0)
+		{
+			n = n * 10 + (*s - '0');
+		}
+
+		else if (n > 0)
+		{
 			break;
-	}
-       	while (*s++);
-	return (num *i);
+		}
+	}while (*s++);
+
+	return (n * i);
 }
