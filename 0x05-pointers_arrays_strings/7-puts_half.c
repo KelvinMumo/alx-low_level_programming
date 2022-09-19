81bb99b4dc16dac062f8d8a3dfd3409e99aa9a02
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-19 16:10:04 +0000

    Last half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -30,3 +30,25 @@ void puts_half(char *str)
 	}
 	_putchar('\n');
 }
+
+/**
+ * _strlen - Returns the length of a string
+ *
+ * @s: String
+ *
+ * Return: 0
+ *
+ */
+int _strlen(char *s)
+{
+	int i, count;
+
+	i = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		i++; 
+	}
+	
+     	return (i);
+}
