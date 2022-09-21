9f16ab12736e3cb30b7f0fb25e83335d2429846c
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-21 16:43:26 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,14 +14,15 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (*(dest + i))
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while ((*(dest + i) = *(src + j)))
+	while (src[j] != '\0')
 	{
-		i++;
+		dest[i] = src[j];
 		j++;
+		i++;
 	}
 	dest[i] = '\0';
 	return (dest);
