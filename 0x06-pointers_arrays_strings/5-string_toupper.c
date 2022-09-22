29c0bb50f6e74536156192bceaa67876b19aa1b2
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-22 11:05:22 +0000

    To Upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,9 +14,9 @@ char *string_toupper(char *s)
 
 	while (s[i] != '\0')
 	{
-		if (s[i] >= 97 && str[i] <= 122)
+		if (s[i] >= 97 && s[i] <= 122)
 		{
-			str[i] = str[i] - 32;
+			s[i] = s[i] - 32;
 		}
 		i++;
 	}
