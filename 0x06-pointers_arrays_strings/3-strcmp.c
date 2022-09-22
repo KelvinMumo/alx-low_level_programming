81ec18fbbf797c39b380298e42fb4b0c21d591c4
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-22 15:59:17 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,12 +13,9 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	for (i = 0; si[i] == s2[i]; i++)
+	while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
 	{
-		if (s1[i] == '\0')
-		{
-			return (0);
-		}
+		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (*(s1 + i) - *(s2 + i));
 }
