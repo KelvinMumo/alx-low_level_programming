225523aca60713eb0a219518c06e5761869ac820
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-22 15:29:33 +0000

    Encode a string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,16 +16,15 @@ char *_strcat(char *dest, char *src)
 
 	i = 0;
 
-	while (dest[i] != '\0')
+	while (*(dest + i))
 	{
 		i++;
 	}
 
 	j = 0;
 
-	while (src[j] != '\0')
+	while ((*(dest + i) = *(src + j)))
 	{
-		dest[i] = src[j];
 		i++;
 		j++;
 	}
