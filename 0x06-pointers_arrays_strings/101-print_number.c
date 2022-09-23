c1b848cbef0d97105d0481fa64606420b8ab2c02
KelvinMumo <kelvinmumo977@gmail.com>
2022-09-23 12:53:43 +0000

    Print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * print_number - Prints an integer
+ *
+ * @n: Integer to be printed
+ *
+ * Return: void
+ *
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
