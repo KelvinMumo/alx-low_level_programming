0b6cb927a7d6e3581c2859060aa6add1d2b00521
KelvinMumo <kelvinmumo977@gmail.com>
2022-10-06 13:48:49 +0000

    string ncat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,12 +17,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		for (i = 0; s1[i] != '\0'; i++)
+			;
 	}
 	if (s2 == NULL)
 		j = 0;
 	else
 	{
 		for (j = 0; s2[j] != '\0'; j++)
+			;
 	}
 	if (j > n)
 		j = n;
