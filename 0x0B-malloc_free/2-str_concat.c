03ad30810c74517bbef91849bf1bfa3938fdd569
KelvinMumo <kelvinmumo977@gmail.com>
2022-10-06 08:55:50 +0000

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,58 +2,37 @@
 
 /**
  * str_concat - Concatenates two strings
- *
  * @s1: First string
  * @s2: Second string
- *
  * Return: pointer to the newly allocated space in memory
  * or NULL on failure
- *
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *a;
-	int i, j;
-	int len1 = 0;
-	int len2 = 0;
+	int i, j,len1 = 0, len2 = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
-
 	a = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (a == NULL)
-	{
 		return (NULL);
-	}
 	i = 0;
 
 	while (i < len1)
-	{
 		a[i] = s1[i];
 		i++;
-	}
 	j = 0;
-
 	while (j <= len2)
-	{
 		a[i] = s2[j];
 		i++;
 		j++;
-	}
 	return (a);
 }
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Returns a pointer to a 2 dimensional array of integers
+ *
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: NULL on failure,
+ * if width or height is zero, return NULL
+ *
