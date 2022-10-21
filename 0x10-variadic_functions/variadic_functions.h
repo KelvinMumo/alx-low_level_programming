6784f9d8c5956853f7f53bb00fb26688defeffde
KelvinMumo <kelvinmumo977@gmail.com>
2022-10-21 15:26:32 +0000

    readme & prototypes

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* _VARIADIC_FUNCTIONS_H_ */
