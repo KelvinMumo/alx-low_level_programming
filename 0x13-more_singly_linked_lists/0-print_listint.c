951b261165794af796dc8725010ad6f8fd95b201
KelvinMumo <kelvinmumo977@gmail.com>
2022-10-27 09:22:26 +0000

    linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,11 +13,10 @@ size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
-		num++;
+		h = h->next, num++;
 	}
 	return (num);
 }
