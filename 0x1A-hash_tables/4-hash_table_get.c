c5350920642c93d9f45ee827c92337e7b99b1e8e
KelvinMumo <kelvinmumo977@gmail.com>
2023-01-08 13:36:32 +0000

    Hash Tables

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves the value associated with a key
+ * @ht: pointer to the hash table
+ * @key: the key
+ *
+ * Return: the value associated with the element,
+ * or NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int index;
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+	{
+		return (NULL);
+	}
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	new_node = ht->array[index];
+
+	while (new_node != NULL)
+	{
+		if (strcmp(new_node->key, key) == 0)
+		{
+			return (new_node->value);
+		}
+		new_node = new_node->next;
+	}
+	return (NULL);
+}
